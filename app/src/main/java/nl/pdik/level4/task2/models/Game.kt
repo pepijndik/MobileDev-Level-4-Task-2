ef7a4216654bca9f6039c7a8916775fea7a83f67
pepijn <pepijn@pdik.nl>
2023-02-20 13:55:18 +0100

    added the task1 model, needs to be changed

--- app/src/main/java/nl/pdik/level4/task2/models/Game.kt
+++ app/src/main/java/nl/pdik/level4/task2/models/Game.kt
@@ -1,3 +1,23 @@
 package nl.pdik.level4.task2.models
 
-data class Game()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "game")
+data class Game(
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "platform")
+    var platform: String,
+
+    @ColumnInfo(name = "release")
+    var release: Date,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long = 0
+
+)
